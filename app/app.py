85c29594774422e6998ffbb258848b589e46e066
Ranjeet <ranjeet.srinivas@gmail.com>
2019-12-26 01:11:13 +0800

    Implemented Flask and mySQL with docker compose

---
+++ app/app.py
@@ -0,0 +1,35 @@
+from typing import List, Dict
+from flask import Flask
+import mysql.connector
+import json
+
+app = Flask(__name__)
+
+
+def campaigns() -> List[Dict]:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'majidDB'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT * FROM campaigns WHERE CAMPAIGN_NAME='Campaign A' LIMIT 100")
+    results = [{CAMPAIGN_NAME: CAMPAIGN_TYPE}
+               for (CAMPAIGN_NAME, CAMPAIGN_TYPE) in cursor]
+    cursor.close()
+    connection.close()
+
+    return results
+
+
+@app.route('/')
+def index() -> str:
+    return json.dumps({'campaigns': campaigns()})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
